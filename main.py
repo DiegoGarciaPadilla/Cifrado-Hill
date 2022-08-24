917d2d41c50891d4b9a8d42efcf0ff5bbbd3265f
Diego Garcia Padilla <gpdiego370@outlook.com>
2022-08-24 13:02:01 -0500

    Encriptar texto

---
+++ main.py
@@ -0,0 +1,66 @@
+# Importar librerías
+
+import numpy as np
+
+# Inicializar variables
+
+abecedario = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", " "]
+
+# Leer mensaje
+
+mensaje = input("Ingrese el mensaje a cifrar: ")
+
+# Convertir mensaje a mayúsculas
+
+mensaje = mensaje.upper()
+
+# Convertir mensaje a lista de numeros
+
+mensaje_numeros = np.array([])
+
+for letra in range(len(mensaje)):
+  if ord(mensaje[letra]) == 32:
+    mensaje_numeros = np.append(mensaje_numeros, 27)
+  else:
+    mensaje_numeros = np.append(mensaje_numeros, (ord(mensaje[letra]) - 64))
+
+# Inicializar matriz de clave
+
+dimClave = int(input("Ingrese la dimensión de la clave: "))
+clave = np.zeros((dimClave, dimClave))
+
+# Ingresar valores de la matriz de clave
+
+for i in range(dimClave):
+    for j in range(dimClave):
+        clave[i][j] = int(input("Ingrese el valor de la posición " + str(i) + "," + str(j) + ": "))
+
+# Validar si la matriz clave es invertible
+
+while np.linalg.det(clave) == 0:
+    print("La matriz clave no es invertible")
+    clave = np.zeros((dimClave, dimClave))
+    for i in range(dimClave):
+        for j in range(dimClave):
+            clave[i][j] = int(input("Ingrese el valor de la posición " + str(i) + "," + str(j) + ": "))
+
+# Initializar matriz de mensaje
+
+if len(mensaje_numeros) % dimClave != 0:
+    espacios = dimClave - (len(mensaje_numeros) % dimClave)
+    for i in range(espacios):
+        mensaje_numeros = np.append(mensaje_numeros, 27)
+
+mensaje_matriz = mensaje_numeros.reshape(int(len(mensaje_numeros) / dimClave), dimClave)
+
+print(mensaje_numeros)
+print(mensaje_matriz)
+
+# Cifrar mensaje
+
+mensaje_cifrado = np.matmul(mensaje_matriz, clave)
+
+# Imprimir mensaje cifrado y guardar en archivo txt
+
+print("Mensaje cifrado: ", mensaje_cifrado.reshape(-1))
+np.savetxt("mensaje_cifrado.txt", mensaje_cifrado.reshape(-1), fmt="%d")
